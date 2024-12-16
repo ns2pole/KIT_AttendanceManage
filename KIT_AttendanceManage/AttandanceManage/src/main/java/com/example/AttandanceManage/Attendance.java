3afd72fffc46f27d95fa0828ab693f122cee62cd
中村俊介 <nakamurashunsuke@nakamurashunsukenoMacBook-Pro.local>
2024-12-16 12:02:30 +0900

    db値表いj

---
+++ KIT_AttendanceManage/AttandanceManage/src/main/java/com/example/AttandanceManage/Attendance.java
@@ -0,0 +1,35 @@
+package com.example.AttandanceManage;
+
+import java.time.LocalDate;
+
+public class Attendance {
+    private int id;
+    private String name;
+    private LocalDate attendanceDate;
+
+    // Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getAttendanceDate() {
+        return attendanceDate;
+    }
+
+    public void setAttendanceDate(LocalDate attendanceDate) {
+        this.attendanceDate = attendanceDate;
+    }
+}
